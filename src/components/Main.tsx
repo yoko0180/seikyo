c1363dcfd2ff549de793bee49ef04262d26b816b
yoko0180 <yoko0180@gmail.com>
2022-06-12 12:42:19 +0900

    update

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -72,8 +72,8 @@ const makeHinmokus = (hinsyu: Hinsyu, labels: string[][]): Hinmoku[] =>
 const def_shop_names: string[][] = [
   ["", ""],
   ["keiou_sinano", "慶応信濃町"],
-  ["keiou_mita", "慶応義塾大学 三田購買部"],
-  ["keiou_kyouritu", "慶応義塾大学芝共立キャンパス"],
+  ["keiou_mita", "慶応 三田"], // 慶応義塾大学 三田購買部
+  ["keiou_kyouritu", "慶応 芝共立"], //慶応義塾大学芝共立キャンパス
   ["keiou_josi", "慶應女子高"],
   ["kango", "日本赤十字看護大学"],
   ["twins", "早稲田twins"],
@@ -81,7 +81,7 @@ const def_shop_names: string[][] = [
   ["hosiyakka", "星薬科大学"],
   ["kaiyou", "海洋大学"],
   ["rika", "理科大学 神楽坂店"],
-  ["housei_iti", "法政大学 市ヶ谷購買書籍部"],
+  ["housei_iti", "法政大学 市ヶ谷"], //法政大学 市ヶ谷購買書籍部
 ]
 
 // 編集モード：品目の数量編集するモード
@@ -315,14 +315,14 @@ const Main: React.FC<{ lang: string }> = ({ lang }) => {
 
   return (
     <div className="App p-5">
-      <h1 className="text-3xl p-1 text-center" id="title">
-        {title}
-      </h1>
+      <h1 className="text-3xl p-1 text-center" id="title">{title}</h1>
 
       {!selectedShop && (
         <div id="add-shop-area" className="border rounded p-2">
           {/* 大学名プルダウン */}
-          <div className="">大学の追加</div>
+          <div className="">
+            大学の追加
+          </div>
           <select
             className="text-black p-3 my-3"
             value={selectShopName}
@@ -362,14 +362,19 @@ const Main: React.FC<{ lang: string }> = ({ lang }) => {
 
       {/* 大学名一覧 */}
       {!selectedShop && (
-        <div id="shops-list" className="border rounded my-2 py-2">
-          <div className="p-2">
-            <span>大学一覧({shops.length})</span>
-            <span className="float-right">
-              total: {shops.map((s) => shopHinmokuTotal(s)).reduce((a, b) => a + b, 0)}
-            </span>
-          </div>
-          {shops.map((shop) => {
+
+      <div id="shops-list" className="border rounded my-2 py-2">
+        <div className="p-2">
+          <span>
+
+          大学一覧({shops.length})
+          </span>
+          <span className="float-right">
+            total: {shops.map((s) => shopHinmokuTotal(s)).reduce((a, b) => a + b, 0)}
+          </span>
+        </div>
+        {
+          shops.map((shop) => {
             return (
               <div key={shop.id} className="my-2">
                 <button className="bg-green-900 p-2 m-1 rounded " onClick={() => setSelectedShop(shop)}>
@@ -389,7 +394,8 @@ const Main: React.FC<{ lang: string }> = ({ lang }) => {
               </div>
             )
           })}
-        </div>
+      </div>
+
       )}
 
       {/* 選択された大学 */}
